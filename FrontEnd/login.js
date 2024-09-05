33bbf3cfe7e55605db3df6a5bfdf5578cd203628
ramollino912 <163904715+ramollino912@users.noreply.github.com>
2024-09-05 08:38:17 -0300

    inicio back e d

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,6 +4,11 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
+    if (!email || !password) {
+        alert('Por favor, ingrese su correo electrónico y contraseña.');
+        return;
+    }
+
     try {
         const response = await fetch('http://localhost:3000/inicio', {
             method: 'POST',
@@ -17,7 +22,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
             alert('Inicio de sesión exitoso');
             window.location.href = 'pagina_principal.html';
         } else {
-            alert('Error: No has podido iniciar sesión, intenta de nuevo.');
+            alert('Correo o contraseña incorrectos. Intente de nuevo.');
         }
     } catch (error) {
         console.error('Error al iniciar sesión:', error);
@@ -25,10 +30,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     }
 });
 
+// Botón de registrarse redirige al formulario de registro
 document.getElementById('registerBtn').addEventListener('click', function() {
     window.location.href = 'pagina_registro.html'; // Redirige a la página de registro
-});
-
-document.getElementById('homeBtn').addEventListener('click', function() {
-    window.location.href = 'pagina_principal.html'; // Redirige a la página principal
 });
