155571a76342b34477a130b736940a7b357c9ba1
RAMOLLINO TOMAS <48792652@ort>
2024-11-10 12:27:41 -0300

    r

--- JavaScript visual code/proyecto/PublicarPerro/script.js
+++ JavaScript visual code/proyecto/PublicarPerro/script.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("registerForm").addEventListener("submit", async function (event) {
-        event.preventDefault();
+document.getElementById('registerForm').addEventListener('submit', async (event) => {
+    event.preventDefault(); // Evita el comportamiento por defecto del formulario
 
+<<<<<<< HEAD
         // Obtener los datos del formulario
         const nombre = document.querySelector('input[name="nombre"]').value;
         const raza = document.querySelector('input[name="raza"]').value;
@@ -66,4 +66,44 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Por favor, selecciona una foto para el perro.');
         }
     });
+=======
+    const token = localStorage.getItem("token");
+
+    const formData = new FormData(event.target); // Crea un objeto FormData a partir del formulario
+    const dogData = {
+        nombre: formData.get("nombre"),
+        raza: formData.get('raza'),
+        color: formData.get('color'),
+        nacimiento: formData.get('nacimiento'),
+        tamaño: formData.get('tamaño'),
+        dificultades: formData.get('dificultades'),
+        descripcion: formData.get("descripcion"),
+        foto: formData.get("foto")
+    };
+
+    console.log(token);
+
+    try {
+        const response = await fetch('http://localhost:3000/perros', {
+            method: 'POST',
+            body: JSON.stringify(dogData),
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (response.ok) {
+            const result = await response.json();
+            console.log('Perro guardado:', result);
+            // Redirigir a otra página donde mostrar los perros
+            window.location.href = 'BusquedaPerro.html';
+        } else {
+            const error = await response.json();
+            console.error('Error al guardar los datos del perro:', error);
+        }
+    } catch (error) {
+        console.error('Error de red:', error);
+    }
+>>>>>>> 4af3d41046082bf4373259575d8c4628337714c9
 });
